9a081e1f7188b6c49f7b7054245838ae2d80477c
john shook <john.a.shook@gmail.com>
2013-07-14 09:48:48 +0000

    modified web.js

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var buffer = new Buffer(16);
+        var message = fs.readFileSync(index.html);
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
