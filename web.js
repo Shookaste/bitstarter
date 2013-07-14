bcee60059eebbd58247bd81120ce742524005899
john shook <john.a.shook@gmail.com>
2013-07-14 10:42:49 +0000

    hh

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var buffer = new Buffer(16);
-    buffer.write(fs.readFileSync("index.html", [encoding]));
+    buffer.write(fs.readFileSync("index.html", "utf8"));
  response.send(buffer.toString("utf-8", 0, 12));
 });
 
