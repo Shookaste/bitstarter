15700077303fc883a66979283002cdcd1ddb8977
john shook <john.a.shook@gmail.com>
2013-07-14 10:48:29 +0000

    h

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     var buffer = new Buffer(50);
     buffer.write(fs.readFileSync("index.html", "utf8"));
- response.send(buffer.toString("utf-8", 0, 50));
+ response.send(buffer.toString("utf-8", 0, 28));
 });
 
 var port = process.env.PORT || 5000;
